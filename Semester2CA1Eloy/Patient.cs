caa7a88b08a3aa9357f0186d9930034d55599911
Eloy Reyes <S00274690@atu.ie>
2025-03-06 22:53:52 +0000

    //DevJournal2 Today it was just for the classes and the enum. I hope the enum works properly like this!

---
+++ Semester2CA1Eloy/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Semester2CA1Eloy.EnumBlood;
+
+namespace Semester2CA1Eloy
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public EnumBlood BloodType { get; set; }
+
+
+        public Patient() { }
+        public Patient(string name, DateTime dob, EnumBlood bloodType)
+        {
+            Name = name;
+            DateOfBirth = dob;
+            BloodType = bloodType;
+        }
+    }
+}
---
+++ Semester2CA1Eloy/Ward.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semester2CA1Eloy
+{
+    public class Ward
+    {
+        // Variable to count for wards, this helps to update the Ward List (x)
+        public static int WardCount { get; private set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        // Patients, using ObservableCollection to update like seen in previous Labs
+        public ObservableCollection<Patient> Patients { get; set; }
+
+        // Constructors
+        public Ward()
+        {
+            Patients = new ObservableCollection<Patient>();
+            WardCount++;
+        }
+        public Ward(string name, int capacity) : this()
+        {
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public override string ToString()
+        {
+            // Name is the "family" like the Marx 
+            return $"{Name} (Limit:{Capacity})";
+        }
+    }
+}
