f2376ddf57999f825a11d7ce68cc3717fc4f9187
Eloy Reyes <S00274690@atu.ie>
2025-03-08 23:29:42 +0000

    //DevJournal3 Another day another c#ollar

--- Semester2CA1Eloy/MainWindow.xaml.cs
+++ Semester2CA1Eloy/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +23,51 @@ namespace Semester2CA1Eloy
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Ward> wards = new ObservableCollection<Ward>();
+       
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
-      private void sliderCapacity_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+
+
+        private void sliderCapacity_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             textBlock2.Text = string.Format("{0:F0}", sliderCapacity.Value);
         }
+
+        //save data
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //string json = JsonConvert.SerializeObject(a, Formatting.Indented);
+
+            using (StreamWriter sw = new StreamWriter(@"c:\temp\Semester2CA1Eloy.json"))
+            {
+                //sw.Write(json);
+
+            }
+        }
+
+        private void tbxPatientName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tbxWardName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnAddWard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddPatient_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
-}//hello
+}
